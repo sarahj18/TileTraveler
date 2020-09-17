13badd2cfc3483705abe0c00da242b17764052f6
Sara Hunfjord <sarahj18@ru.is>
2020-09-17 13:49:47 +0000

    changes to tiletraveler

---
+++ tiletraveler.py
@@ -0,0 +1,6 @@
+# A computer game in which the player is located in a certain tile in a grid
+# the player chooses directions and the program tells him/her if the direction is valid or not
+# the goal is to reach the victory location (3,1).
+
+# 
+
